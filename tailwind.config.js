5d5a6b82b54e264ee8fe7880b582432568adc9df
Benat Asban S <143315026+BenatAsban@users.noreply.github.com>
2026-06-16 18:32:55 +0530

    Add Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4F46E5',
+        secondary: '#A78BFA',
+        accent: '#10B981',
+      },
+    },
+  },
+  darkMode: 'class',
+  plugins: [],
+}
